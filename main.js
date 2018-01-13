ff952f22d82732e652ca77249b3c52d93fcb434b
Azu <abbey-1825@hotmail.com>
2018-01-12 21:30:56 -0300

    Making it more responsive

--- main.js
+++ main.js
@@ -13,45 +13,68 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById("operations").innerHTML += content;
 
             if (!signs.indexOf(content)) {
+
                 lastOperation = content;
             }
 
             if (content == "=") {
 
                 try {
+
                     eval(operations);
+
                 } catch (err) {
-                    console.log(result);
+
                     document.getElementById("entry").innerHTML = "Syntax ERROR";
                     document.getElementById("operations").innerHTML = "";
+
                 } finally {
                     var result = eval(operations);
                 }
 
                 if (typeof (result) == "number") {
+
                     if (result == "Infinity" || result == "NaN") {
+
                         document.getElementById("entry").innerHTML = "Math ERROR";
                         document.getElementById("operations").innerHTML = "";
+
                     } else {
+
+                        var decimals;
+
+                        if (result % 1 != 0) {
+
+                            result = result.toFixed(3);
+                        }
+
                         document.getElementById("entry").innerHTML = result;
                         document.getElementById("operations").innerHTML += result;
                     }
                 }
 
             } else if (content == "AC") {
+
                 operations = "";
                 content = "";
                 document.getElementById("entry").innerHTML = 0;
                 document.getElementById("operations").innerHTML = "";
+
             } else if (content == "x") {
+
                 operations += "*";
+
             } else if (content == "CE") {
+
                 var NewOp = operations.slice(0, operations.indexOf(lastOperation));
                 operations = NewOp;
                 document.getElementById("entry").innerHTML = 0;
                 document.getElementById("operations").innerHTML = operations;
+
             } else {
+
                 operations += content;
+
             }
 
         })
